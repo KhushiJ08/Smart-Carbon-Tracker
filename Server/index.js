a5aeca67a865ebd172b413bb597b8113bf80a397
Khushi <khushijatolia05@gmail.com>
2026-08-08 14:25:36 +0530

    Prepare server for Heroku deployment

--- Server/index.js
+++ Server/index.js
@@ -11,7 +11,8 @@ app.get("/", (req, res) => {
   res.send("Smart Carbon Tracker Backend Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
